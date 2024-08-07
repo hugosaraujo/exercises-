a0bf3a3bf4fec8ece2fe290fdcfb91c4f9fd03f8
Hugo de Araujo <hugo.saraujo@tutanota.com>
2024-08-07 20:58:16 -0300

    Adicionando perguntas do desafio 4

---
+++ exercises4.py
@@ -0,0 +1,8 @@
+# 1- Crie um dicionario representando informações sobre uma pessoa, como nome, idade e cidade. 
+# 2- Utilizando o dicionario criado no item anterior: 
+## -> Modifique o valor de um dos itens no dicionário(por exemplo, atualize a idade da pessoa);
+## -> Adicione um campo de profissão à pessoa;
+## -> Remova um item do dicionario;
+# 3- Crie um dicionário utilizando para representar números e seus quadrados de 1 a 5.
+# 4- Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário. 
+# 5- Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionario. 
