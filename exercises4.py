3085a610fb3027b8a8eaa4fa7741a62eb856f50e
Hugo de Araujo <hugo.saraujo@tutanota.com>
2024-08-08 19:38:36 -0300

    Resolução da quinta pergunta do programa dos exercicios

--- exercises4.py
+++ exercises4.py
@@ -21,14 +21,32 @@ pessoas[0]['idade'] = 28
 
 
 # 3- Crie um dicionário utilizando para representar números e seus quadrados de 1 a 5.
-
+'''
 numeros = {x: x**2 for x in range(1, 6)}
 print(numeros)
+'''
 
-# 4- Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário. 
+# 4- Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário.
+'''
+
 if 'nome' in pessoas:
     print('Chave encontrada')
 else:
     print('Chave não encontrada')
 
+'''
+
 # 5- Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionario. 
+
+frase = f'''
+Give it away, give it away, give it away now
+Give it away, give it away, give it away now
+Give it away, give it away, give it away now
+I can't tell if I'm a kingpin or a pauper
+'''.lower()
+
+contagem_palavras = {}
+palavras = frase.split()
+for palavra in palavras: 
+    contagem_palavras[palavra] = contagem_palavras.get(palavra, 0) + 1
+print(contagem_palavras)
