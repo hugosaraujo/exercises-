48703fe3e9df7db7115786c7d2862dda0e62a71a
Hugo de Araujo <hugo.saraujo@tutanota.com>
2024-08-07 21:21:20 -0300

    Resolução de exercícios de 1 a 4

--- exercises4.py
+++ exercises4.py
@@ -1,8 +1,34 @@
-# 1- Crie um dicionario representando informações sobre uma pessoa, como nome, idade e cidade. 
+# 1- Crie um dicionario representando informações sobre uma pessoa, como nome, idade e cidade.
+pessoas = [{'nome': 'Lucas', 'idade': 27, 'cidade': 'São Paulo'}, 
+          {'nome': 'João', 'idade': 30, 'cidade':'Rio de Janeiro'},
+          {'nome': 'Alice', 'idade': 4, 'cidade': 'Brasília'}]
+
 # 2- Utilizando o dicionario criado no item anterior: 
 ## -> Modifique o valor de um dos itens no dicionário(por exemplo, atualize a idade da pessoa);
+pessoas[0]['idade'] = 28
+#print(pessoa[0])
 ## -> Adicione um campo de profissão à pessoa;
+
+#for i in range(len(pessoas)):
+#    pessoas[i]['profissao'] = 'Desenvolvedor'
+#    print(pessoas[i])
+
 ## -> Remova um item do dicionario;
+
+#for i in range(len(pessoas)):
+#    del pessoas[i]['idade']
+#    print(pessoas[i])
+
+
 # 3- Crie um dicionário utilizando para representar números e seus quadrados de 1 a 5.
+
+numeros = {x: x**2 for x in range(1, 6)}
+print(numeros)
+
 # 4- Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário. 
+if 'nome' in pessoas:
+    print('Chave encontrada')
+else:
+    print('Chave não encontrada')
+
 # 5- Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionario. 
